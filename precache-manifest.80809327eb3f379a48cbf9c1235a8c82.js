b97eb30aba54b3f66905feaa86dbd39a24e539c8
Sebastiaan Jansen <whitebird@live.Be>
2019-01-16 10:01:04 +0100

    Updates

---
+++ precache-manifest.80809327eb3f379a48cbf9c1235a8c82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18655e749c95fc4c1ce0",
+    "url": "/colorclock/static/js/runtime~main.18655e74.js"
+  },
+  {
+    "revision": "4011d6fbb6f6950c18d3",
+    "url": "/colorclock/static/js/main.4011d6fb.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/colorclock/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "4011d6fbb6f6950c18d3",
+    "url": "/colorclock/static/css/main.6426425b.chunk.css"
+  },
+  {
+    "revision": "f0edf1d1cc114fb8a652c50d4f8353a5",
+    "url": "/colorclock/index.html"
+  }
+];
