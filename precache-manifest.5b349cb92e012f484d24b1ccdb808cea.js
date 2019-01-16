9f7fa6ee7ebec1ec590b12d14bac80bbafffaba4
Sebastiaan Jansen <whitebird@live.Be>
2019-01-16 09:59:56 +0100

    Updates

---
+++ precache-manifest.5b349cb92e012f484d24b1ccdb808cea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2aa9d5b4e1bc415b2531",
+    "url": "/static/js/main.2aa9d5b4.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "2aa9d5b4e1bc415b2531",
+    "url": "/static/css/main.6426425b.chunk.css"
+  },
+  {
+    "revision": "a3e7c04cdaf9954a0e773903e79f8c8c",
+    "url": "/index.html"
+  }
+];
